b5567d45863068c6b99074eb936e3c0ad9c8f441
EyeTyrant <1414jg@gmail.com>
2020-01-22 18:47:49 -0600

    17.4.2 Exercise complete?

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -2,17 +2,23 @@ package org.launchcode.codingevents.models;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class EventCategory {
 
+  @Id
+  @GeneratedValue
   private int id;
-  private String name;
 
+  private String name;
 
   public EventCategory(String name) {
     this.name = name;
   }
+
+  public EventCategory() {}
 
   public String getName() {
     return name;
